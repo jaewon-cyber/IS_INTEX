5fec0c76d8fb0a77a99cca39e0c4c1daece2c830
홍길동 <@naver.com>
2025-12-01 15:14:17 -0700

    changed login

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const path = require('path'); 
+app.use(express.static(path.join(__dirname, 'public')));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
 const port = process.env.PORT || 3000;
+const session = require("express-session");
 
 const session = require("express-session");
 
@@ -50,6 +51,13 @@ app.post("/login", async (req, res) => {
   res.redirect("/");
 });
 
+  // Render Login Page
+  app.get("/login", (req, res) => {
+      const error = req.session.error || null; 
+      req.session.error = null; 
+      res.render("login", { error: error });
+  });
+  
 // LOGOUT
 app.get("/logout", (req, res) => {
   req.session.destroy();
